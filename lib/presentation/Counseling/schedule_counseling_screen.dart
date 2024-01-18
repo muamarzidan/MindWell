7de6032e698ee291f580b952e5dd2678dbee1336
muamarzidan <codeofomiru11@gmail.com>
2024-01-18 07:07:25 +0700

    feat: adjust schedule

--- lib/presentation/Counseling/schedule_counseling_screen.dart
+++ lib/presentation/Counseling/schedule_counseling_screen.dart
@@ -21,35 +21,129 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
             const IconThemeData(size: 30.0, color: AppColors.primaryFontColor),
         actions: const [],
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 200,
-              height: 200,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color.fromARGB(29, 147, 207, 255),
-              ),
-              child: const Icon(
-                Icons.notifications,
-                color: Color.fromARGB(240, 255, 179, 0),
-                size: 120,
-              ),
-            ),
+            _listSchedule(),
             const SizedBox(height: 10),
-            const Text(
-              'Don t have a notification yetDD',
-              style: TextStyle(
-                color: AppColors.secondaryFontColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
-              ),
-            ),
-            const SizedBox(height: 100),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _listSchedule() {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 253, 246, 246),
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 226, 226, 226).withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.1,
+            height: 70,
+            child: ClipRRect(
+              borderRadius:
+                  BorderRadius.circular(35.0),
+              child: Image.asset(
+                'assets/images/main/psikolog/Psikolog1_Rina.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          const Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Kafka Nafiza. M.Psi. Psikolog',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.primaryFontColor,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Jadwal Sesi',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.secondaryFontColor,
+                          ),
+                        ),
+                        Text(
+                          'Senin, 18 Desember 2023',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.secondaryFontColor,
+                          ),
+                        ),
+                        Text(
+                          'Selasa, 19 Desember 2023',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.secondaryFontColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Waktu Sesi',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.secondaryFontColor,
+                          ),
+                        ),
+                        Text(
+                          '17.00 - 18.00 WIB',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.secondaryFontColor,
+                          ),
+                        ),
+                        Text(
+                          '21.00 - 22.00 WIB',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.secondaryFontColor,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
